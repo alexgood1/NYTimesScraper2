81d8822681dde2ba2790ec9d5cd720e1cc7e2f4b
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-01 20:33:44 -0700

    MVP fully functional, date, and author, not displayed currently for convenienrther the use o further the implementation of the API the program would need to change a little bit so that those could be easily displayed given the response

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -7,10 +7,7 @@ router.route("/")
   .post(articlesController.create);
 
 // Matches with "/api/articles/:id"
-router
-  .route("/:id")
-  .get(articlesController.findById)
-  .put(articlesController.update)
+router.route("/:id")
   .delete(articlesController.remove);
 
 module.exports = router;
